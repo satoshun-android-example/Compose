f39873b511e72c50b40f7ebe9fda7b6d9d6d1d0e
SatoShun <shun.sato1@gmail.com>
2020-02-20 05:29:14 +0000

    spacer

--- app/src/main/java/com/github/satoshun/example/ExampleApp.kt
+++ app/src/main/java/com/github/satoshun/example/ExampleApp.kt
@@ -2,8 +2,16 @@ package com.github.satoshun.example
 
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.layout.LayoutPadding
+import androidx.ui.layout.Row
+import androidx.ui.layout.Spacer
+import androidx.ui.unit.dp
 
 @Composable
 fun ExampleApp() {
-  Text("first")
+  Row(modifier = LayoutPadding(top = 16.dp)) {
+    Text("first")
+    Spacer(modifier = LayoutPadding(left = 16.dp))
+    Text("second")
+  }
 }
