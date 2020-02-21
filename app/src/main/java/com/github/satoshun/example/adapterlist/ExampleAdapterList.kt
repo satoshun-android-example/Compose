acb971910c0d378c36cd28c0214cdadbf1bfba1e
SatoShun <shun.sato1@gmail.com>
2020-02-21 13:59:00 +0000

    ripple

--- app/src/main/java/com/github/satoshun/example/adapterlist/ExampleAdapterList.kt
+++ app/src/main/java/com/github/satoshun/example/adapterlist/ExampleAdapterList.kt
@@ -6,16 +6,19 @@ import androidx.ui.foundation.AdapterList
 import androidx.ui.foundation.Clickable
 import androidx.ui.layout.LayoutPadding
 import androidx.ui.layout.Row
+import androidx.ui.material.ripple.Ripple
 import androidx.ui.unit.dp
 
 @Composable
 fun ExampleAdapterList() {
   AdapterList((0..100).toList()) {
-    Clickable(onClick = { }) {
-      Row(modifier = LayoutPadding(16.dp)) {
-        //      val image = imageResource(R.drawable.img)
+    Ripple(bounded = false) {
+      Clickable(onClick = { }) {
+        Row(modifier = LayoutPadding(16.dp)) {
+          //      val image = imageResource(R.drawable.img)
 //      Container(modifier = LayoutSize(40.dp, 40.dp)) { DrawImage(image) }
-        Text("$it")
+          Text("$it")
+        }
       }
     }
   }
