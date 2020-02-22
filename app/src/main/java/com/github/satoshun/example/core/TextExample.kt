9a0405d93ff28385597450f73b779f663ed3835c
SatoShun <shun.sato1@gmail.com>
2020-02-22 11:00:02 +0000

    text

--- app/src/main/java/com/github/satoshun/example/core/TextExample.kt
+++ app/src/main/java/com/github/satoshun/example/core/TextExample.kt
@@ -1,11 +1,14 @@
 package com.github.satoshun.example.core
 
 import androidx.compose.Composable
+import androidx.ui.core.LayoutTag
 import androidx.ui.core.Text
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutHeight
 import androidx.ui.layout.Row
 import androidx.ui.layout.Spacer
+import androidx.ui.layout.constraintlayout.ConstraintLayout
+import androidx.ui.layout.constraintlayout.ConstraintSet
 import androidx.ui.text.TextStyle
 import androidx.ui.unit.dp
 import androidx.ui.unit.sp
@@ -17,10 +20,29 @@ fun TextExample() {
       text = "あああああああああああああああああああああああああああああああああああ",
       style = TextStyle(fontSize = 20.sp)
     )
+
     Spacer(modifier = LayoutHeight(24.dp))
+
     Row {
       Text(
-        text = (0..50).map { "い" }.joinToString(""),
+        text = (0..50).joinToString("") { "い" },
+        style = TextStyle(fontSize = 20.sp)
+      )
+    }
+
+    Spacer(modifier = LayoutHeight(24.dp))
+
+    ConstraintLayout(constraintSet = ConstraintSet {
+      val title = tag("title")
+      title.apply {
+        top constrainTo parent.top
+        left constrainTo parent.left
+        right constrainTo parent.right
+      }
+    }) {
+      Text(
+        modifier = LayoutTag("title"),
+        text = (0..50).joinToString("") { "う" },
         style = TextStyle(fontSize = 20.sp)
       )
     }
