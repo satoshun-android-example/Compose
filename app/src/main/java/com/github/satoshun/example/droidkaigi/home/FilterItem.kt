3c3291ee010e55a52fa590db7203154b4a5be9a7
SatoShun <shun.sato1@gmail.com>
2020-02-24 03:18:29 +0000

    filter

--- app/src/main/java/com/github/satoshun/example/droidkaigi/home/FilterItem.kt
+++ app/src/main/java/com/github/satoshun/example/droidkaigi/home/FilterItem.kt
@@ -1,21 +1,27 @@
 package com.github.satoshun.example.droidkaigi.home
 
 import androidx.compose.Composable
-import androidx.ui.core.Alignment
 import androidx.ui.core.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
+import androidx.ui.graphics.vector.DrawVector
+import androidx.ui.layout.Arrangement
 import androidx.ui.layout.Column
 import androidx.ui.layout.Container
 import androidx.ui.layout.LayoutPadding
+import androidx.ui.layout.LayoutSize
 import androidx.ui.layout.LayoutWidth
+import androidx.ui.layout.Row
+import androidx.ui.layout.Spacer
 import androidx.ui.material.Divider
 import androidx.ui.material.ripple.Ripple
 import androidx.ui.material.surface.Surface
+import androidx.ui.res.vectorResource
 import androidx.ui.text.TextStyle
 import androidx.ui.text.font.FontWeight
 import androidx.ui.unit.dp
 import androidx.ui.unit.sp
+import com.github.satoshun.example.R
 
 @Composable
 fun HomeFilterItem() {
@@ -25,12 +31,16 @@ fun HomeFilterItem() {
       shape = RoundedCornerShape(topLeft = 24.dp)
     ) {
       Column {
-        Container(
+        Row(
           modifier = LayoutPadding(top = 18.dp, bottom = 18.dp, right = 24.dp) + LayoutWidth.Fill,
-          alignment = Alignment.CenterRight
+          arrangement = Arrangement.End
         ) {
-          //  val icon = vectorResource(id = R.drawable.ic_baseline_filter_list_24)
-          //  DrawVector(icon, tintColor = Color.Black)
+          Container(modifier = LayoutSize(22.dp)) {
+            val icon = vectorResource(id = R.drawable.ic_baseline_filter_list_24)
+            DrawVector(icon, tintColor = Color.Black)
+          }
+
+          Spacer(modifier = LayoutWidth(8.dp))
 
           Ripple(bounded = false) {
             Text(
