1891772fe1dd3db8a350e60baa5cf40e6889452f
SatoShun <shun.sato1@gmail.com>
2020-02-27 01:02:01 +0000

    state2

--- app/src/main/java/com/github/satoshun/example/compose/StateExample.kt
+++ app/src/main/java/com/github/satoshun/example/compose/StateExample.kt
@@ -4,17 +4,46 @@ import androidx.compose.Composable
 import androidx.compose.state
 import androidx.ui.core.Text
 import androidx.ui.foundation.Clickable
+import androidx.ui.layout.Column
+import androidx.ui.layout.Container
+import androidx.ui.layout.LayoutPadding
 import androidx.ui.material.ripple.Ripple
+import androidx.ui.unit.dp
 
 @Composable
 fun StateExample() {
+  val (count2, count2Callback) = state { 0 }
   var count by state { 0 }
 
-  Ripple(bounded = false) {
-    Clickable(onClick = {
-      count += 1
-    }) {
-      Text(text = count.toString())
+  Column {
+    println("1")
+    Ripple(bounded = false) {
+      println("2")
+      Clickable(onClick = {
+        println("onClick")
+        count += 1
+      }) {
+        println("3")
+        Container(LayoutPadding(24.dp)) {
+          println("4")
+          Text(text = count.toString())
+        }
+      }
+    }
+
+    println("11")
+    Ripple(bounded = false) {
+      println("22")
+      Clickable(onClick = {
+        println("onClick")
+        count2Callback(count2 + 1)
+      }) {
+        println("33")
+        Container(LayoutPadding(24.dp)) {
+          println("44")
+          Text(text = count2.toString())
+        }
+      }
     }
   }
 }
